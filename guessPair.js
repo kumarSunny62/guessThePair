1f82ecb368e972c24996a18e48c21d080eab39ea
kumarSunny62 <sunnysunny621513@gmail.com>
2025-01-21 17:23:39 +0530

    xyg

--- guessPair.js
+++ guessPair.js
@@ -6,11 +6,11 @@ let img5 = 'images/five.jpg'
 let img6 = 'images/six.jpg'
 let img7 = 'images/seven.jpg'
 let img8 = 'images/eight.jpg'
-const pathArray = [img1, img2, img3, img4, img5, img6, img7, img8];
+const pathForImageArray = [img1, img2, img3, img4, img5, img6, img7, img8];
 let imgArr = [];
-for (let i = 0; i < pathArray.length; i++) {
+for (let i = 0; i < pathForImageArray.length; i++) {
     let img = new Image();
-    img.src = pathArray[i];
+    img.src = pathForImageArray[i];
     imgArr.push(img);
 
 }
@@ -21,7 +21,7 @@ start_button.addEventListener('click', start_game);
 reset_button.addEventListener('click', reset_game);
 function start_game() {
     console.log('game started');
-    //.....show img first few second will figure out how to do that.
+    //.....show img first few second will figure out how to do that...
     setInterval(stop_watch, 1000);
     loadImg();
 
@@ -32,33 +32,38 @@ function reset_game() {
 
 }
 
+let time = 0;
 function stop_watch() {
     document.getElementById('timer').innerText = ` ${time++} seconds`
 }
-let time = 0;
 function loadImg() {
-    let numbers = [0, 1, 2, 3, 4, 5, 6, 7];
-    let button_random_array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-    for (let i = numbers.length - 1; i > 0; i--) {
+    let randomIndexForImage = [0, 1, 2, 3, 4, 5, 6, 7]; //[2,0,4,]
+    let randomIndexForButton = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] //[3,6,1,,.......]
+    for (let i = randomIndexForImage.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
+        [randomIndexForImage[i], randomIndexForImage[j]] = [randomIndexForImage[j], randomIndexForImage[i]];
     }
-    for (let i = 0; i < button_random_array.length; i++) {
+    for (let i = 0; i < randomIndexForButton.length; i++) {
         const j = Math.floor(Math.random() * (i + 1));
-        [button_random_array[i], button_random_array[j]] = [button_random_array[j], button_random_array[i]];
+        [randomIndexForButton[i], randomIndexForButton[j]] = [randomIndexForButton[j], randomIndexForButton[i]];
     }
-    // console.log(numbers);
-    console.log(button_random_array);
+    // console.log(randomIndexForImage);
+    console.log(randomIndexForButton);
 
     let buttons = document.getElementsByClassName('inner_grid');
     console.log(buttons);
     let a = 0;
     for (let i = 0; i < 8; i++) {
-        // console.log(button_random_array[a++])
-        // console.log(button_random_array[a++])
-        addImageToButtons(buttons[button_random_array[a++]], numbers[i]); // 0 , 0
+        // console.log(randomIndexForButton[a++])
+        // console.log(randomIndexForButton[a++])
+        // console.log({a})
+        addImageToButtons(buttons[randomIndexForButton[a++]], randomIndexForImage[i]); // 0 , 0  i=0  bra=3  ni=2
+        // console.log(randomIndexForButton[a], "randomIndexForButton[a] - 1", randomIndexForImage[i])
         // console.log("a1 : ",a,b,i);
-        addImageToButtons(buttons[button_random_array[a++]], numbers[i]);// 1, 0
+        // console.log({a})
+        addImageToButtons(buttons[randomIndexForButton[a++]], randomIndexForImage[i]);// 1, 0
+        // console.log({a});
+        
         // console.log("a1 : ",a,c,i);
     }
 
@@ -67,11 +72,13 @@ function addImageToButtons(button, randomNumber) {
     // console.log(button,randomNumber, 'appended');
     // button.innerHtml = 
     let newdiv = document.createElement('div')
+    newdiv.classList.add(randomNumber)
     // newdiv.innerHTML= imgArr[randomNumber]
+    // console.log({randomNumber})
     newdiv.append(imgArr[randomNumber])
 
-    console.log(imgArr[randomNumber], 'img')
-    // console.log(newdiv, 'newdiv');
+    // console.log(imgArr[randomNumber], randomNumber, 'img')
+    console.log(newdiv);
 
     button.append(newdiv);
 
